c6c6a99581076a1777050f7e46ced3d9326df4a3
arrudafranco <arrudafranco@uchicago.edu>
2020-11-11 23:49:28 -0600

    Commented the analysis script. Markdown document with analysis remains.

--- data_analysis.R
+++ data_analysis.R
@@ -2,9 +2,9 @@ library(ggplot2)
 library(RColorBrewer)
 
 evangelicals <- c(2, 3, 4, 5)
-main_religions <- c(1, 2, 97)
 dataset_condensed <- mutate(full_dataset,
                             religion_condensed = ifelse(S_700 %in% evangelicals, 2, S_700))
+#Combining the four types of evangelicals was more representative for my purposes.
 
 year_count_table <- full_dataset %>%
   group_by(X_002) %>%
@@ -17,6 +17,7 @@ religion_count <- dataset_condensed %>%
   left_join(year_count_table, by = 'X_002') %>%
   rename(religion_obs = n) %>%
   mutate(religion_yr_prop = religion_obs / year_count) %>%
+  #Filtering out very small religions.
   filter(religion_yr_prop >= 0.05)
 
 ggplot(religion_count) +
@@ -28,11 +29,16 @@ ggplot(religion_count) +
   scale_fill_brewer(palette = 'Dark2',
                     labels = c('1' = 'Catholic', '2' = 'Evangelical', '97' = 'No Religion'))
 
+#Through the bar chart, I was able to detect that the main religious categories by far are
+#Catholics, Evangelicals and Non-Religious. So I created the following list for future reference.
+main_religions <- c(1, 2, 97)
+
 support_democracy_count <- dataset_condensed %>%
   group_by(religion_condensed, X_002, A_001_001) %>%
   count() %>%
-  inner_join(religion_count) %>%
+  inner_join(religion_count, by = c("religion_condensed", "X_002")) %>%
   mutate(dem_support_prop = n / religion_obs) %>%
+  #Filtering out NAs and very small religions.
   filter(religion_condensed %in% main_religions & A_001_001 %in% c(1, 2, 3))
 
 ggplot(support_democracy_count) +
@@ -45,7 +51,6 @@ ggplot(support_democracy_count) +
   scale_color_brewer(palette = 'Dark2',
                      labels = c('1' = 'Catholic', '2' = 'Evangelical', '97' = 'No Religion'))
 
-#Line of averages for the scales. Easier visualization for time changes.
 dataset_condensed %>%
   filter(A_003_031 > 0 & S_700 %in% main_religions) %>%
   group_by(X_002, religion_condensed) %>%
